9e79e24537628f3e489ecf0142ad45634c4e610c
David Davó <david@ddavo.me>
2019-11-16 13:24:22 +0100

    Entregable 3.1

--- Pr3/MySystem/my_system.c
+++ Pr3/MySystem/my_system.c
@@ -1,5 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int system(const char * command) {
+    int pid;
+    int ret;
+
+    if( (pid = fork()) == -1) {
+        fprintf(stderr, "Error forking\n");
+    } else if (pid == 0) {
+        char * const parmList[] = {command, NULL};
+        execvp(command, parmList);
+        // These two lines should not execute, so if they are running, we have
+        // a problem
+        fprintf(stderr, "Error executing %s\n", command);
+        exit(127);
+    }
+
+    while (wait(&ret) != -1);
+
+    return ret;
+}
 
 int main(int argc, char* argv[])
 {
