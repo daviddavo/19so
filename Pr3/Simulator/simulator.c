5819b83b13b3300c3a5ace133fd9d2260d4f6231
David Davó <david@ddavo.me>
2019-11-17 10:39:29 +0100

    Started simulator

--- Pr3/Simulator/simulator.c
+++ Pr3/Simulator/simulator.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define N_PARADS 5
+#define EN_RUTA 0
+#define EN_PARADA 1
+#define MAX_USUARIOS 40
+#define USUARIO 4
+
+int estado = EN_RUTA;
+int parada_actual = 0;
+int n_ocupantes = 0;
+
+// personas que desean subir en cada parada
+int esperando_parada[N_PARADAS];
+int esperando_bajar[N_PARADAS];
+
 int main(int argc, char* argv[])
 {
 	/* ... To be completed ... */
